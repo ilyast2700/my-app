709d5a55a7e572714f34a24fb19404a50c86a255
ilyast2700 <44866736+ilyast2700@users.noreply.github.com>
2019-04-23 21:27:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ class Square extends React.Component {
   render() {
     return (
       <button
-        className="square"
-        onClick={ _ => this.setState({value: 'X'})}
+        className = "square"
+        onClick = {() => this.setState({value: 'X'})}
       >
           {this.state.value}
       </button>
@@ -31,7 +31,12 @@ class Board extends React.Component {
 }
 
 renderSquare(i) {
-  return <Square value={this.state.squares[i]} />;
+  return (
+    <Square
+      value = {this.state.squares[i]}
+      onClick = {() => this.handleClick(i)}
+      />
+    );
 }
 
   render() {
@@ -39,18 +44,18 @@ renderSquare(i) {
 
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
+        <div className = "status">{status}</div>
+        <div className = "board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className="board-row">
+        <div className = "board-row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className="board-row">
+        <div className = "board-row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
@@ -63,11 +68,11 @@ renderSquare(i) {
 class Game extends React.Component {
   render() {
     return (
-      <div className="game">
-        <div className="game-board">
+      <div className = "game">
+        <div className = "game-board">
           <Board />
         </div>
-        <div className="game-info">
+        <div className = "game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
