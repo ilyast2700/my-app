75d8d056614337e26e9b39d8583e5b82afcc3843
ilyast2700 <44866736+ilyast2700@users.noreply.github.com>
2019-04-23 22:30:15 +0300

    Update index.js
    add handleClick

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Square extends React.Component {
     return (
       <button
         className = "square"
-        onClick = {() => this.props.onClick({value: 'X'})}
+        onClick = {() => this.props.onClick()}
       >
           {this.props.value}
       </button>
@@ -21,16 +21,22 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
     };
-}
+  }
 
-renderSquare(i) {
-  return (
-    <Square
-      value = {this.state.squares[i]}
-      onClick = {() => this.handleClick(i)}
-      />
-    );
-}
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value = {this.state.squares[i]}
+        onClick = {() => this.handleClick(i)}
+        />
+      );
+    }
 
   render() {
     const status = 'Next player: X';
